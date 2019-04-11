75ee1c4132e92ac35c5d3676e23d0aa865f60ff7
Aleksandr Ivanov <aleksaivanov@ozon.ru>
2019-04-11 20:03:02 +0300

    Added triangle inequality rule.

---
+++ ExcellentGeometry/TriangleInequalityRule.cs
@@ -0,0 +1,16 @@
+﻿
+using System;
+
+namespace ExcellentGeometry
+{
+    internal static class TriangleInequalityRule
+    {
+        internal static bool IsSatisfied(
+            double sizeA,
+            double sizeB,
+            double sizeC)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
